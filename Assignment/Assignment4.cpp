c9674bcdd38dfe4243005c828cec6c0b474a8569
cherrymaxx0 <ariksab007@gmail.com>
2026-04-18 23:57:37 +0600

    = +=Assignment operator overloading and ++increment added

--- Assignment/Assignment4.cpp
+++ Assignment/Assignment4.cpp
@@ -13,33 +13,33 @@ using namespace std;
 //     }
 // };
 // class Substract{
-// public:
-//     int a;
-//     Substract(){
-//         cin>>a;
-//     }
-//     void operator-(Substract bb){
-//         a-=bb.a;
+    // public:
+    //     int a;
+    //     Substract(){
+        //         cin>>a;
+        //     }
+        //     void operator-(Substract bb){
+            //         a-=bb.a;
 //     }
 // };
 // class Multiply{
 // public:
 //     int a;
 //     Multiply(){
-//         cin>>a;
-//     }
+    //         cin>>a;
+    //     }
 //     void operator*(Multiply kk){
-//         a*=kk.a;
-//     }
-// };
+    //         a*=kk.a;
+    //     }
+    // };
 // class Divide{
-// public:
-//     int a;
-//     Divide(){
-//         cin>>a;
-//     }
-//     void operator/(Divide nn){
-//         a/=nn.a;
+    // public:
+    //     int a;
+    //     Divide(){
+        //         cin>>a;
+        //     }
+        //     void operator/(Divide nn){
+            //         a/=nn.a;
 //     }
 // };
 
@@ -54,20 +54,71 @@ using namespace std;
 //     mm/nn; cout<<"Division: "<<mm.a<<endl;
 // }
 
-//"=" Assignment Operator Overloading
-class Assign{
-public:
+// //"=" Assignment Operator Overloading
+// class Assign{
+    // public:
+    //     int a;
+//     Assign(){
+    //         cin>>a;
+    //     }
+    //     void operator=(Assign nn){
+//         a=nn.a;
+//     }
+// };
+// int main(void){
+    //     Assign xx,yy;
+    //     cout<<"XX object then: "<<xx.a<<endl;
+//     xx=yy;
+//     cout<<"XX object after assigning: "<<xx.a<<endl;
+// }
+
+// //"+=" Plus assignment operator overloading
+// class AddAssign{
+//     public:
+//     int a;
+//     AddAssign(){
+//         cin>>a;
+//     }
+//     void operator+=(AddAssign yy){
+//         a+=yy.a;
+//     }
+// };
+// int main(void){
+//     AddAssign xx,yy;
+//     cout<<"XX object then: "<<xx.a<<endl;
+//     cout<<"YY object then: "<<yy.a<<endl;
+//     xx+=yy;
+//     cout<<"XX object after assigning: "<<xx.a<<endl;
+//     cout<<"YY object after assigning: "<<yy.a<<endl;
+// }
+
+//
+class PrefixIncrement{
+    public:
     int a;
-    Assign(){
+    PrefixIncrement(){
         cin>>a;
     }
-    void operator=(Assign nn){
-        a=nn.a;
+    void operator++(){
+        ++a;
+    }
+};
+class PostfixIncrement{
+    public:
+    int a;
+    PostfixIncrement(){
+        cin>>a;
+    }
+    void operator++(int){
+        a++;
     }
 };
 int main(void){
-    Assign xx,yy;
-    cout<<"XX object then: "<<xx.a<<endl;
-    xx=yy;
+    PrefixIncrement xx;
+    ++xx;
     cout<<"XX object after assigning: "<<xx.a<<endl;
+    
+    PostfixIncrement yy;
+    yy++;
+    cout<<"YY object after assigning: "<<yy.a<<endl;
 }
